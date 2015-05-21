08d0cc6b7ae1d9fac6e02eb626620bf755a7bff3
mzlevy <mzlevy@users.noreply.github.com>
2015-05-20 23:06:54 -0400

    Update server.R
    Trimmed to just a function to compute the eigen value and right eigenvector of a Leslie Matrix

--- server.R
+++ server.R
@@ -1,71 +1,33 @@
-require(shiny)
 
-shinyServer(function(input, output) {
-  calcs <- reactive(function(){
-    A0 <- matrix(0, nrow=5, ncol=5)
-    A0[1, 2] <- input$fc2 # fecundity, class 2
-    A0[1, 3] <- input$fc3 # fecundity, class 3
-    A0[1, 4] <- input$fc4 # fecundity, class 4
-     A0[1, 5] <- input$fc4 # fecundity, class 5
-    A0[2, 1] <- input$tp1 # transition probability, class 1 to 2
-    A0[3, 2] <- input$tp2 # transition probability, class 2 to 3
-    A0[4, 3] <- input$tp3 # transition probability, class 3 to 4
-    A0[5, 4] <- input$sp4 # transition probability, class 4 to 5
-    A0[5, 5] <- input$sp4 # survival probability, class 5
+ 
+  leslie<-function(f12,f13,f14,f15,f16,t12,t23,t34,t45,t56,t66) {
+    A0 <- matrix(0, nrow=6, ncol=6)
+    A0[1, 2] <- f12 # fecundity, class 2
+    A0[1, 3] <-  f13# fecundity, class 3
+    A0[1, 4] <- f14  # fecundity, class 4
+     A0[1, 5] <- f15  # fecundity, class 5
+     A0[1, 6] <- f16  # fecundity, adult
+    A0[2, 1] <- t12 # transition probability, class 1 to 2
+    A0[3, 2] <- t23 # transition probability, class 2 to 3
+    A0[4, 3] <- t34 # transition probability, class 3 to 4
+    A0[5, 4] <- t45 # transition probability, class 4 to 5
+    A0[6, 5] <- t56 # transition probability, class 5 to adult
+    A0[6, 6] <- t66 # survival probability, adult
     lambda <- eigen(A0)$values[1] # dominant eigenvalue
     rvec <- eigen(A0)$vectors[, 1] # right eigenvector
     lvec <- eigen(t(A0))$vectors[, 1] # left eigenvector
-    emat <- array(dim=c(5, 5)) # elasticity matrix
-    for (i in 1:5){
-      for (j in 1:5){
-        emat[i, j] <- A0[i, j] * lvec[i] * rvec[j] / (lambda * t(lvec) %*% rvec)
-      }
-    }
-    emat <- matrix(as.numeric(emat), nrow=4, ncol=4)
-    rownames(emat) <- c("Class 1", "Class 2", "Class 3", "Class 4", "Class 5")
-    colnames(emat) <- rownames(emat)
-    rownames(A0) <- rownames(emat)
-    colnames(A0) <- rownames(emat)
-    x0 <- c(10, 0, 0, 0,0) # initial pop'n
-    x <- array(NA, dim=c(5, input$timesteps))
-    x[,1] <- x0
-    Nt <- array(NA, dim=ncol(x)) # total population
-    px <- array(dim=c(5, ncol(x))) # proportion in each age class
-    for (t in 2:input$timesteps){
-      x[,t] <- A0 %*% x[, t-1]
-      Nt[t] <- sum(x[, t])
-      px[,t] <- x[, t]/Nt[t]
-    }
-    
-   list(A0=A0, emat=emat, x=x, Nt=Nt, px=px, Lambda=lambda)
-  })
 
-  output$elastTab <- reactiveTable(function(){
-   calcs()$emat
-   }
-  )
-  
-  output$Lesliemat <- reactiveTable(function(){
-    calcs()$A0
-    }
-  )
-  
-  output$plot <- reactivePlot(function(){
-    out <- calcs()
-    with(out, {
-      title <- substitute(paste("Dominant eigenvalue = ", Lambda,
-                                 sep=""), list(Lambda=as.numeric(Lambda)))
-         plot(x=1:ncol(x), y=Nt, type="l", xlab="Time", ylab="Log(population size)", lty=2, log="y", 
-              main=title)
-         lines(x=1:ncol(x), y=x[1, ], col="purple")
-         lines(x=1:ncol(x), y=x[2, ], col="red")
-         lines(x=1:ncol(x), y=x[3, ], col="dark orange")
-         lines(x=1:ncol(x), y=x[4, ], col="blue")
-         lines(x=1:ncol(x), y=x[5, ], col="dark green")
-         ypos <- max(abs(Nt), na.rm=T)
-         legend(x=1, y=ypos, c("Class 1", "Class 2", "Class 3", "Class 4", "Class 5"), 
-                col=c("purple", "red", "dark orange", "blue", "dark green"),
-                text.col="black", lty=c(1, 1, 1, 1, 1))
-    })
-  })
-})
+    
+#   out<-list(A0=A0, emat=emat, x=x, Nt=Nt, px=px, Lambda=lambda)
+   out<-list(A0=A0, Lambda=lambda, stablestagedistribution=rvec)
+   return(out)
+  }
+
+#example run function
+#fecundity only occurs in adult stage, 20 offspring
+leslie(0,0,0,0,20,.7,.7,.8,.8,.8,.9)
+
+
+
+
+
